49ec48b140f80de3cabac55c33a1e5c66792671c
kshashank22 <kshashank.rinku@gmail.com>
2022-12-23 09:48:52 +0000

    first commit

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,40 @@
+// Write your code here.
+import './index.css'
+import {Component} from 'react'
+
+class ThumbnailsList extends Component {
+  renderThumbnails = () => {
+    const {imagesList, onClickUpdateId, selectedImageId} = this.props
+    return imagesList.map(eachItem => {
+      const {thumbnailUrl, id, thumbnailAltText} = eachItem
+      const onClickThumbnail = () => {
+        onClickUpdateId(id)
+      }
+      const buttonName =
+        id === selectedImageId ? 'selected-list-item' : 'list-item'
+      return (
+        <li key={id} className={buttonName} onClick={onClickThumbnail}>
+          <button className="button" type="button">
+            <img
+              src={thumbnailUrl}
+              className="thumbnail-image"
+              alt={thumbnailAltText}
+            />
+          </button>
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="thumbnail-container">
+        <h1 className="heading">Nature Photography</h1>
+        <p className="description">Nature Photography by Rahul</p>
+        <ul className="thumbnails-list-container">{this.renderThumbnails()}</ul>
+      </div>
+    )
+  }
+}
+
+export default ThumbnailsList
